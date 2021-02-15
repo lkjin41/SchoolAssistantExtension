e3915d8a33b6f736b51da6a3f33d6afe9d924cb4
Artemiy Prikupets <artemprikupets@gmail.com>
2021-02-15 20:24:13 +0300

    Update(schoolmosreg.js): now it can show multiple GDZs for one subject

--- schoolmosreg.js
+++ schoolmosreg.js
@@ -9,17 +9,50 @@ let schedule = [
 ];
 
 let gdzBySubjects = {
-  "Англ. язык": "https://gdz.ru/class-9/english/reshebnik-spotlight-9-vaulina-yu-e/",
-  "Геометрия": "https://gdz.ru/class-7/geometria/atanasyan-7-9/",
-  "Алгебра": "https://gdz.ru/class-9/algebra/mordkovich-10",
-  "Физика": "https://www.euroki.org/gdz/ru/fizika/9_klass/a_v_peryshkin_e_m_gutnik",
-  "Химия": "https://gomolog.ru/reshebniki/9-klass/kuznecova-2019.html",
-  "Информатика": "https://5urokov.ru/gdz/bosova_9_uch",
-  "ОБЖ": "https://gdzznaniya.ru/obzh-9/smirnov/",
-  "Ист.России.Всеоб.ист": "https://megaresheba.ru/gdz/istorija/9-klass/arsentjev",
-  "География": "https://www.euroki.org/gdz/ru/geografiya/9_klass/konturnye-karty-po-geografii-9-klass-fgos-673",
-  "Рус. язык": "https://gdz.ru/class-9/russkii_yazik/barhudarov-kruchkov-9/",
-  "Родной язык": "https://gdz.ru/class-9/russkii_yazik/barhudarov-kruchkov-9/"
+  "Англ. язык": {
+    "Учебник": "https://gdz.ru/class-9/english/reshebnik-spotlight-9-vaulina-yu-e/",
+    "Рабочая тетрадь": "https://gdz.ru/class-9/english/spotlight-workbook/"
+  },
+  "Геометрия": {
+    "Учебник": "https://gdz.ru/class-7/geometria/atanasyan-7-9/"
+  },
+  "Алгебра": {
+    "Учебник": "https://gdz.ru/class-9/algebra/mordkovich-10"
+  },
+  "Физика": {
+    "Учебник": "https://www.euroki.org/gdz/ru/fizika/9_klass/a_v_peryshkin_e_m_gutnik"
+  },
+  "Химия": {
+    "Учебник": "https://gomolog.ru/reshebniki/9-klass/kuznecova-2019.html"
+  },
+  "Информатика": {
+    "Учебник": "https://5urokov.ru/gdz/bosova_9_uch"
+  },
+  "ОБЖ": {
+    "Учебник": "https://gdzznaniya.ru/obzh-9/smirnov/"
+  },
+  "Ист.России.Всеоб.ист": {
+    "Учебник": "https://megaresheba.ru/gdz/istorija/9-klass/arsentjev"
+  },
+  "География": {
+    "Учебник": "https://www.euroki.org/gdz/ru/geografiya/9_klass/konturnye-karty-po-geografii-9-klass-fgos-673",
+    "Контурные карты": "https://www.euroki.org/gdz/ru/geografiya/9_klass/konturnye-karty-po-geografii-9-klass-fgos-673"
+  },
+  "Рус. язык": {
+    "Учебник": "https://gdz.ru/class-9/russkii_yazik/barhudarov-kruchkov-9/"
+  },
+  "Родной язык": {
+    "Учебник": "https://gdz.ru/class-9/russkii_yazik/barhudarov-kruchkov-9/"
+  },
+  "Литература": {
+    "Учебник": "https://gdz.ru/class-9/literatura/korovina/"
+  },
+  "Биология": {
+    "Учебник": "https://gdz.ru/class-9/biologiya/ponomareva/"
+  },
+  "Обществознание": {
+    "Учебник": "https://gdz.ru/class-9/obshhestvoznanie/bogolyubov/"
+  }
 }
 
 function start() {
@@ -34,8 +67,12 @@ function start() {
 
   function insertGdzButton(underLessonTitleElement) {
     let subject = underLessonTitleElement.parentElement.getElementsByClassName("strong")[0].title;
-    let gdzURL = gdzBySubjects[subject];
-    underLessonTitleElement.innerHTML += "<a target=\"_blank\" rel=\"noopener noreferrer\" href=\"" + gdzURL + "\" style=\"color: white; background-color: #49b1dc; padding: 1px 6px;\">ГДЗ</a>";
+    if(gdzBySubjects[subject] != undefined) {
+      for (var gdzName in gdzBySubjects[subject]) {
+        let gdzURL = gdzBySubjects[subject][gdzName];
+        underLessonTitleElement.innerHTML += "<a target=\"_blank\" rel=\"noopener noreferrer\" href=\"" + gdzURL + "\" style=\"color: white; background-color: #49b1dc; display: flex; padding: 5px 6px;\">ГДЗ " + gdzName + "</a>";
+      }
+    }
   }
 
   let days = document.getElementsByClassName("grid vam marks");
@@ -43,7 +80,7 @@ function start() {
     let underLessonTitleElements = day.getElementsByClassName("light");
     for(let i = 0; i < underLessonTitleElements.length; i++) {
       let underLessonTitleElement = underLessonTitleElements[i];
-      underLessonTitleElement.style = "white-space: nowrap;";
+      underLessonTitleElement.style = "white-space: nowrap; display: table-caption; margin-top: 5px;";
       replaceLessonNumberTitleHTML(underLessonTitleElement);
       insertSchedule(underLessonTitleElement, i);
       insertGdzButton(underLessonTitleElement);
